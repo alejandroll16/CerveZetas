68d069a23e813a0c048f9a24adbdb5afdb9c322b
ElZetas <94991541+ElZetas@users.noreply.github.com>
2023-04-26 00:41:39 +0200

    Update CerveZetas_DML.sql

--- CerveZetas_DML.sql
+++ CerveZetas_DML.sql
@@ -4,12 +4,12 @@
 /*Tabla Marca*/
 
 
-INSERT INTO CerveZetas_Marca (codigo, nombre) values ('001', 'Alhambra');
-INSERT INTO CerveZetas_Marca (codigo, nombre) values ('002', 'Paulaner');
-INSERT INTO CerveZetas_Marca (codigo, nombre) values ('003', 'Mahou Maestra');
-INSERT INTO CerveZetas_Marca (codigo, nombre) values ('004', 'Estrella Levante');
-INSERT INTO CerveZetas_Marca (codigo, nombre) values ('005', 'Patronus');
-INSERT INTO CerveZetas_Marca (codigo, nombre) values ('006', 'Voll Damn');
+INSERT INTO CerveZetas_Marca (codigo, nombre) values (001, 'Alhambra');
+INSERT INTO CerveZetas_Marca (codigo, nombre) values (002, 'Paulaner');
+INSERT INTO CerveZetas_Marca (codigo, nombre) values (003, 'Mahou Maestra');
+INSERT INTO CerveZetas_Marca (codigo, nombre) values (004, 'Estrella Levante');
+INSERT INTO CerveZetas_Marca (codigo, nombre) values (005, 'Patronus');
+INSERT INTO CerveZetas_Marca (codigo, nombre) values (006, 'Voll Damn');
 
 
 
@@ -37,18 +37,18 @@ values ('pablob@gmail.com', 'Pablo', 'Beltrán', '114', 600000004, SYSDATE, TO_D
 
 
 INSERT INTO CerveZetas_Producto (cod_producto, stock, descripcion, admin_crea, admin_modifica, fecha_mod, fecha_creacion, Categoria, precio, TipoProducto, marca) 
-values ('010', 60, 'Happy Holidays', 'pabloverdu@admin.es', 'pabloverdu@admin.es', SYSDATE, SYSDATE, 'Rubia', 20.75, 'Pack_Especial', '001');
+values (010, 60, 'Happy Holidays', 'pabloverdu@admin.es', 'pabloverdu@admin.es', SYSDATE, SYSDATE, 'Rubia', 20.75, 'Pack_Especial', 001);
 
 INSERT INTO CerveZetas_Producto (cod_producto, stock, descripcion, admin_crea, admin_modifica, fecha_mod, fecha_creacion, Categoria, precio, TipoProducto, marca) 
-values ('020', 70, 'Sant Patrick`s', 'pabloverdu@admin.es', 'pabloverdu@admin.es', SYSDATE, SYSDATE, 'Malta', 14.23, 'Pack_Eventual', '002');
+values (020, 70, 'Sant Patrick`s', 'pabloverdu@admin.es', 'pabloverdu@admin.es', SYSDATE, SYSDATE, 'Malta', 14.23, 'Pack_Eventual', 002);
 
 /*Cambiar admin_modifica por pabloverdu@admin.es*/
 
 INSERT INTO CerveZetas_Producto (cod_producto, stock, descripcion, admin_crea, admin_modifica, fecha_mod, fecha_creacion, Categoria, precio, TipoProducto, marca) 
-values ('030', 80, 'Fin de Examenes', 'pabloverdu@admin.es', 'pablob@gmail.com', SYSDATE, SYSDATE, 'Rubia', 5.25, 'Cubos', '003');
+values (030, 80, 'Fin de Examenes', 'pabloverdu@admin.es', 'pablob@gmail.com', SYSDATE, SYSDATE, 'Rubia', 5.25, 'Cubos', 003);
 
 INSERT INTO CerveZetas_Producto (cod_producto, stock, descripcion, admin_crea, admin_modifica, fecha_mod, fecha_creacion, Categoria, precio, TipoProducto, marca) 
-values ('040', 200, 'Cerveza dignidad','pabloverdu@admin.es', 'pabloverdu@admin.es', SYSDATE, SYSDATE, 'Tostada', 2.5, 'Cervezas','005');
+values (040, 200, 'Cerveza dignidad','pabloverdu@admin.es', 'pabloverdu@admin.es', SYSDATE, SYSDATE, 'Tostada', 2.5, 'Cervezas',005);
 
 
 
@@ -95,6 +95,7 @@ values (131, 2424, '020', 40.75, 2);
 UPDATE CerveZetas_Usuario SET tipo_usuarios = 'Cliente' WHERE email = 'mario@gmail.com';
 UPDATE CerveZetas_Direccion SET Municipio = 'Petrer', CodigoPostal = 03610 WHERE email_usuario = 'mario@gmail.com';
 UPDATE CerveZetas_Producto SET admin_modifica = 'pabloverdu@admin.es' WHERE cod_producto = '030';
+UPDATE CerveZetas_Marca SET nombre = 'Mohou' WHERE codigo = 003;
 
 
 
