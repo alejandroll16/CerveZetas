7289d8afa6831c8f8ccadec237031136e2ee360f
ElZetas <94991541+ElZetas@users.noreply.github.com>
2023-04-26 00:52:29 +0200

    Update CerveZetas_DML.sql

--- CerveZetas_DML.sql
+++ CerveZetas_DML.sql
@@ -2,8 +2,6 @@
 
 
 /*Tabla Marca*/
-
-
 INSERT INTO CerveZetas_Marca (codigo, nombre) values (001, 'Alhambra');
 INSERT INTO CerveZetas_Marca (codigo, nombre) values (002, 'Paulaner');
 INSERT INTO CerveZetas_Marca (codigo, nombre) values (003, 'Mahou Maestra');
@@ -18,16 +16,16 @@ INSERT INTO CerveZetas_Marca (codigo, nombre) values (006, 'Voll Damn');
 
 
 
-INSERT INTO CerveZetas_Usuario (email, nombre, apellidos, contraseña, telefono, ultima_conexion, fecha_nacimiento, foto, tipo_usuarios) 
+INSERT INTO CerveZetas_Usuario (email, nombre, apellidos, contrasena, telefono, ultima_conexion, fecha_nacimiento, foto, tipo_usuarios) 
 values ('pabloverdu@admin.es', 'Pablo', 'Verdu', '1234', 600000001, SYSDATE, TO_DATE('31/12/2001', 'DD/MM/YYYY'), 'URL_IMAGEN', 'Admin');
 
-INSERT INTO CerveZetas_Usuario (email, nombre, apellidos, contraseña, telefono, ultima_conexion, fecha_nacimiento, foto, tipo_usuarios, metodo_pago) 
+INSERT INTO CerveZetas_Usuario (email, nombre, apellidos, contrasena, telefono, ultima_conexion, fecha_nacimiento, foto, tipo_usuarios, metodo_pago) 
 values ('mario@gmail.com', 'Mario', 'Martínez', '112', 600000002, SYSDATE, TO_DATE('16/12/2001', 'DD/MM/YYYY'), 'URL_IMAGEN', 'Admin', 'PayPal');
 
-INSERT INTO CerveZetas_Usuario (email, nombre, apellidos, contraseña, telefono, ultima_conexion, fecha_nacimiento, foto, tipo_usuarios, metodo_pago) 
+INSERT INTO CerveZetas_Usuario (email, nombre, apellidos, contrasena, telefono, ultima_conexion, fecha_nacimiento, foto, tipo_usuarios, metodo_pago) 
 values ('pablon@gmail.com', 'Pablo', 'Navarro', '113', 600000003, SYSDATE, TO_DATE('12/07/2001', 'DD/MM/YYYY'), 'URL_IMAGEN', 'Cliente', 'Tarjeta');
 
-INSERT INTO CerveZetas_Usuario (email, nombre, apellidos, contraseña, telefono, ultima_conexion, fecha_nacimiento, foto, tipo_usuarios) 
+INSERT INTO CerveZetas_Usuario (email, nombre, apellidos, contrasena, telefono, ultima_conexion, fecha_nacimiento, foto, tipo_usuarios) 
 values ('pablob@gmail.com', 'Pablo', 'Beltrán', '114', 600000004, SYSDATE, TO_DATE('03/05/2001', 'DD/MM/YYYY'), 'URL_IMAGEN', 'Visitante');
 
 
